9bb90a7bb41afe64638a0f6997bbbe4b3a73c6e9
akshat7593 <akshat.agrawal@iiitb.org>
2019-01-17 10:05:38 +0530

    adding addition module

---
+++ caladd.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class caladd{
+        public static void main(String args[]){
+                float a,b,res;
+                char choice,ch;
+                Scanner scan=new Scanner(System.in);
+
+                do{
+                        System.out.println("1.Add 2 numbers\n");
+                        System.out.println("2.Exit \n\n");
+                        System.out.println("Enter Your Choice: ");
+                        choice=scan.next().charAt(0);
+                        switch(choice){
+                                case '1':System.out.println("Enter Two Number:");
+                                         a=scan.nextFloat();
+                                         b=scan.nextFloat();
+                                         res=a+b;
+                                         System.out.println("Result ="+res);
+                                         break;
+				case '2':System.exit(0);
+                                         break;
+                                }
+                                System.out.println("\n...............\n");
+        }while(choice!=2);
+}
+}
+
